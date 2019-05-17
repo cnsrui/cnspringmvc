e2cd2b2e6391c007ca55acfab7d8f0f3e05078b5
cnsrui <cnsrui@vip.qq.com>
2019-05-17 23:18:48 +0800

    rebuild cnspringmvc with View Resolver

--- src/main/java/com.cnspringmvc.demo/controller/SaveProductController.java
+++ src/main/java/com.cnspringmvc.demo/controller/SaveProductController.java
@@ -2,46 +2,43 @@ package com.cnspringmvc.demo.controller;
 
 import com.cnspringmvc.demo.domain.Product;
 import com.cnspringmvc.demo.form.ProductForm;
-import com.cnspringmvc.demo.validator.ProductValidator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 
 public class SaveProductController implements Controller {
 
+    private static final Log logger = LogFactory
+            .getLog(SaveProductController.class);
+
     @Override
-    public String handleRequest(HttpServletRequest request,
-                                HttpServletResponse response) {
+    public ModelAndView handleRequest(HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
+        logger.info("SaveProductController called");
         ProductForm productForm = new ProductForm();
         // populate action properties
         productForm.setName(request.getParameter("name"));
         productForm.setDescription(request.getParameter("description"));
         productForm.setPrice(request.getParameter("price"));
 
-        // validate ProductForm
-        ProductValidator productValidator = new ProductValidator();
-        List<String> errors = productValidator.validate(productForm);
-        if (errors.isEmpty()) {
-            // create Product from ProductForm
-            Product product = new Product();
-            product.setName(productForm.getName());
-            product.setDescription(productForm.getDescription());
+        // create model
+        Product product = new Product();
+        product.setName(productForm.getName());
+        product.setDescription(productForm.getDescription());
+        try {
             product.setPrice(Float.parseFloat(productForm.getPrice()));
-
-            // no validation error, execute action method
-            // insert code to save product to the database
-
-            // store product in a scope variable for the view
-            request.setAttribute("product", product);
-            return "/WEB-INF/jsp/ProductDetails.jsp";
-        } else {
-            // store errors and form in a scope variable for the view
-            request.setAttribute("errors", errors);
-            request.setAttribute("form", productForm);
-            return "/WEB-INF/jsp/ProductForm.jsp";
+        } catch (NumberFormatException e) {
         }
+
+        // insert code to save Product
+        // 视图与模型一个返回
+        //return new ModelAndView("/WEB-INF/jsp/ProductDetails.jsp", "product", product);
+        return new ModelAndView("ProductDetails", "product", product);
     }
 
 }
