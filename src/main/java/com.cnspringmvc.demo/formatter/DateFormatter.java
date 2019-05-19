4742dcbe021503eb3a1c5872efa115efe982050d
cnsrui <cnsrui@vip.qq.com>
2019-05-19 12:24:44 +0800

    Employee project could relove SimpleDateFormat with Conventer

---
+++ src/main/java/com.cnspringmvc.demo/formatter/DateFormatter.java
@@ -0,0 +1,41 @@
+package com.cnspringmvc.demo.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateFormatter implements Formatter<Date> {
+
+    private String datePattern;
+    private SimpleDateFormat dateFormat;
+
+    public DateFormatter(String datePattern) {
+        this.datePattern = datePattern;
+        dateFormat = new SimpleDateFormat(datePattern);
+        dateFormat.setLenient(false);
+    }
+
+    @Override
+    public String print(Date date, Locale locale) {
+
+        return dateFormat.format(date);
+    }
+    /**
+     * 该方法是在何时执行的呢？
+     * 是在Controller中路径映射的方法中，默认识别日期字段并执行吗？
+     */
+    @Override
+    public Date parse(String s, Locale locale) throws ParseException {
+        try {
+            return dateFormat.parse(s);
+        } catch (ParseException e) {
+            // the error message will be displayed when using <form:errors>
+            throw new IllegalArgumentException(
+                    "invalid date format. Please use this pattern\""
+                            + datePattern + "\"");
+        }
+    }
+}
